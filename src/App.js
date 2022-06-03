c7aea4ed96938aa271fe7de2cbfbdd4a6eeb2971
Martha Quintanilla-Ramirez <marthaquin21@gmail.com>
2022-06-02 23:11:01 -0700

    completed lab 11

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import BestBooks from './BestBooks';
+import About from './About';
+import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   BrowserRouter as Router,
@@ -22,6 +24,11 @@ class App extends React.Component {
             >
             </Route>
             {/* PLACEHOLDER: add a route with a path of '/about' that renders the `About` component */}
+            <Route 
+              path="/about"
+              element={<About />}
+            >
+            </Route>
           </Routes>
           <Footer />
         </Router>
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import  Carousel  from 'react-bootstrap/Carousel';
+import  Image  from 'react-bootstrap/Image';
+import bookImg from './lib.jpeg';
+import axios from 'axios';
+import { Container } from 'react-bootstrap';
+
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -9,20 +15,56 @@ class BestBooks extends React.Component {
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+componentDidMount = async () => {
+  try{
 
+    const config = {
+      method: 'get',
+      baseURL: process.env.REACT_APP_SERVER,
+      url: '/books'
+    }
+    const response = await axios(config);
+    this.setState({
+      books: response.data
+    })
+  }catch(error){
+    console.error('Error in BestBooks componentDidMount: ', error);
+    this.setState({
+      errorMessage: `Status Code: ${error.response.status}: ${error.response.data}`
+    })
+  }
+}
   render() {
 
     /* TODO: render all the books in a Carousel */
 
     return (
       <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        <h2 id="title">My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        <Container>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
+        {this.state.books.length ? ( // this is saying IF this.state.books exist, if its more than 0 then give me the carousel.
+          <Carousel id="carousel">
+              {this.state.books.map(book => (
+                <Carousel.Item key={book.title} >
+              <Image id="carousel-image"
+                className="w-100"
+                src={bookImg}
+                alt={book.title}
+                
+                />
+                <Carousel.Caption id="carousel-text-box">
+                <h2 className="carousel-text">{book.title}</h2>
+                <p className="carousel-text">{book.description}</p>
+                <p className="carousel-text">Status: {book.status}</p>
+              </Carousel.Caption>
+            </Carousel.Item>
+                ))}
+          </Carousel>
+          ) : ( // if NOT then give me "no books found"
+          <h3 id="no-books">No Books Found :(</h3>
+          )}
+          </Container>
       </>
     )
   }
